74361a0bdd6d818d90ae5b32648b8655eccb60dd
Cpt-Graby <alexisgonelle.mobile@gmail.com>
2023-07-15 21:15:46 +0200

    Modification du bot afin qu'il precise l'user qui achete des choses

--- inventory.py
+++ inventory.py
@@ -1,55 +1,99 @@
 class Item:
     def __init__(self, name: str, qty: int):
+        """
+        Initializes an Item object.
+        Args:
+            name (str): The name of the item.
+            qty (int): The initial quantity of the item.
+        """
         self.name = name
         self.qty = qty
         self.qty_sold = 0
+        self.alias = []
+
 
 class Inventory:
     def __init__(self):
+        """
+        Initializes an Inventory object.
+        """
         self.inventory = {}
-        pass
+        self.alias = []
 
     def add_item(self, nameup: str, qty: int):
         """
-        add_item permet de rajouter des elements dans l'inventaire de l'item.
+        Adds items to the inventory of an item.
+        Args:
+            nameup (str): The name of the item to add.
+            qty (int): The quantity of the item to add.
         """
         name = nameup.lower()
-        if (qty < 0):
+        if qty < 0:
             pass
-        if (name in self.inventory):
-            print("Already exist.\n")
+        if name in self.inventory:
+            print("Already exists.\n")
             self.inventory[name].qty_sold += qty
-            print(f"Qty:{qty} added.\n")
+            print(f"Qty: {qty} added.\n")
             pass
         item = Item(name, qty)
         self.inventory[name] = item
 
     def achat_item(self, nameup: str, qty: int = 1):
+        """
+        Increases the quantity sold of an item.
+        Args:
+            nameup (str): The name of the item.
+            qty (int): The quantity to add to the quantity sold. Default is 1.
+        """
         name = nameup.lower()
         if (qty < 0):
             pass
-        if (name not in self.inventory):
+        if name not in self.inventory:
             print("Does not exist")
             pass
         self.inventory[name].qty_sold += qty
 
     def correct_error(self, nameup: str, qty: int = 1):
+        """
+        Corrects the quantity sold of an item.
+        Args:
+            nameup (str): The name of the item.
+            qty (int): The quantity to subtract from the quantity sold. Default is 1.
+        """
         name = nameup.lower()
         if (qty < 0):
             pass
-        if (name not in self.inventory):
+        if name not in self.inventory:
             print("Does not exist")
             pass
-        if (self.inventory[name].qty_sold - qty >= 0):
+        if self.inventory[name].qty_sold - qty >= 0:
             self.inventory[name].qty_sold -= qty
 
     def __str__(self):
+        """
+        Returns a string representation of the inventory.
+        Returns:
+            str: The inventory items and their quantities sold.
+        """
         new_bilan = ""
         for key in self.inventory.keys():
-            new_bilan += f"{key} : {self.inventory[key].qty_sold}\n"
-        return (new_bilan)
+            new_bilan += f"{key}: {self.inventory[key].qty_sold}\n"
+        return new_bilan
 
     def reset_inventory(self):
-        #print(f"{self}")
+        """
+        Resets the quantities sold of all items in the inventory.
+        """
         for key in self.inventory.keys():
             self.inventory[key].qty_sold = 0
+    
+    def item_exists(self, nameup: str) -> bool:
+        """
+        Vérifie si un élément existe dans l'inventaire.
+        Args:
+            nameup (str): Le nom de l'élément à vérifier.
+        Returns:
+            bool: True si l'élément existe dans l'inventaire, False sinon.
+        """
+        name = nameup.lower()
+        return name in self.inventory
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 from inventory import *
 
 logger = settings.logging.getLogger("bot")
+log_channel = 1129829426922791122
 
 def init_inventaire_BDE():
     inventaire = Inventory()
@@ -35,23 +36,31 @@ def main():
         logger.info(f"User:{bot.user} (ID: {bot.user.id})")
 
     @bot.command(aliases=['a'])
-    async def achat(ctx, nameup: str, qty: int = 1):
+    async def achat(ctx, name: str, qty: int = 1):
         """Permet d'annoncer que tu achetes quelques choses aux BDE """
-        name = nameup.lower()
-        Inventaire.achat_item(name, qty)
-        await ctx.send(f"{name}: {qty} achete")
+        channel_log = bot.get_channel(log_channel)
+        if (Inventaire.item_exists(name) == True):
+            Inventaire.achat_item(name, qty)
+            await channel_log.send(f"{ctx.author} - {name}: {qty} achat")
+        else:
+            await ctx.send(f"Doesn't exist")
+
 
     @bot.command(aliases=['re'])
-    async def rendu(ctx, nameup: str, qty: int = 1):
-        name = nameup.lower()
+    async def rendu(ctx, name: str, qty: int = 1):
         """Permet d'annoncer une erreur"""
-        Inventaire.correct_error(name, qty)
-        await ctx.send(f"{name}: {qty} rendu")
+        channel_log = bot.get_channel(log_channel)
+        if (Inventaire.item_exists(name) == True):
+            Inventaire.correct_error(name, qty)
+            await channel_log.send(f"{ctx.author} - {name}: {qty} rendu")
+        else:
+            await ctx.send(f"Doesn't exist")
 
     @bot.command(hidden=True)
     async def reset(ctx):
-        await ctx.send(f"{Inventaire}")
-        Inventaire.reset_inventory(self)
+        channel_log = bot.get_channel(log_channel)
+        await channel_log.send(f"{ctx.author} is ressetting the invertoy:\n{Inventaire}")
+        Inventaire.reset_inventory()
 
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
 
